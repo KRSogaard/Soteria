2d7f0df4ae4d11cce9cd25928ba3987ee73292f9
Kasper Sogaard <kasper@sogaard.us>
2022-02-02 07:52:06 -0500

    Spring boot

---
+++ src/test/java/com/wewelo/Soteria/SoteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wewelo.Soteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoteriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
